cc8f9e1158455ea44e984956bedea11f04e2536e
BEmanuel87 <bucur.emanuel87@gmail.com>
2026-05-10 17:33:42 +0300

    Add HowItWorksSection section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/Navbar";
 import FeaturesSection from "@/components/sections/FeaturesSection";
 import HeroSection from "@/components/sections/HeroSection";
+import HowItWorksSection from "@/components/sections/HowItWorksSection";
 import LogosSection from "@/components/sections/LogosSection";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection />
         <LogosSection />
         <FeaturesSection />
+        <HowItWorksSection />
       </main>
     </>
   );
